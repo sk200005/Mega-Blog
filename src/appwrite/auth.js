d022861ccdbb418a62b0a4035f049321218498bc
sk200005 <swayamkorde2005@gmail.com>
2026-02-22 13:14:39 +0530

    Add authentication functionality

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,5 +1,7 @@
-import conf from '../conf/conf'
+import conf from '../conf/conf.js'
 import { Client, Account, ID } from "appwrite";
+//Client → connects your app to Appwrite server
+//Account → handles authentication (login, signup, session, logout)
 
 export class AuthService {
 
@@ -9,25 +11,60 @@ export class AuthService {
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
 
-        this.account = new Account(this.client);
+        this.account = new Account(this.client);  
+        //Links authentication methods to your configured client
     }
 
     async createAccount({ email, password, name }) {
         try {
             const userAccount = await this.account.create(
-                ID.unique(),
+                userId: ID.unique(),
                 email,
                 password,
                 name
-            );
-
-            return userAccount;
+             );
+            if(userAccount)  {
+                //another function
+                return this.login({email,password})
+            }
+            else {return userAccount;}
+            
 
         } catch (error) {
             throw error;
         }
+        
+        
+    }
+
+    async login ({email, password}){
+         try {
+            return await this.account.createEmailPasswordSession({
+                email : email,
+                password : password
+            })
+         } catch (error) {
+            throw(error)
+         }
+    }
+
+    async getCurrentUser (){
+        try {
+            return  await this.account.get();
+        } catch (error) {
+            console.log ("Appwritr error :: getCurrentUser :" + error)
+        }
+
+        return null;
+    }
+
+    async logout (){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log ("Appwritr error :: logout :" + error)
+        }
     }
 }
-
 const authService = new AuthService();
 export default authService;
